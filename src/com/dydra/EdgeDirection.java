6e1fa5d5f8823332d964226afedaa065b8647172
Arto Bendiken <arto@bendiken.net>
2011-07-24 18:46:20 +0200

    Implemented the com.dydra.EdgeDirection enum.

---
+++ src/com/dydra/EdgeDirection.java
@@ -0,0 +1,13 @@
+/* This is free and unencumbered software released into the public domain. */
+
+package com.dydra;
+
+/**
+ * The directionality of a graph edge.
+ *
+ * @see http://docs.dydra.com/sdk/java
+ * @see http://en.wikipedia.org/wiki/Edge_(graph_theory)
+ */
+public enum EdgeDirection {
+  OUTGOING, INCOMING, BOTH
+}
