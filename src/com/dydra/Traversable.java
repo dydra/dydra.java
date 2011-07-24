e5fa58a923d03bb55fe2860547df9ce681b3827c
Arto Bendiken <arto@bendiken.net>
2011-07-24 18:25:06 +0200

    Imported a stub for the com.dydra.Traversable interface.

---
+++ src/com/dydra/Traversable.java
@@ -0,0 +1,16 @@
+/* This is free and unencumbered software released into the public domain. */
+
+package com.dydra;
+
+import java.util.Iterator;
+
+/**
+ * Implementing this interface enables an object to be used in a
+ * <code>foreach<code> statement for traversing nodes.
+ *
+ * @see http://docs.dydra.com/sdk/java
+ * @see http://en.wikipedia.org/wiki/Graph_traversal
+ */
+public interface Traversable extends Iterable<Node> {
+  public Iterator<Node> iterator();
+}
