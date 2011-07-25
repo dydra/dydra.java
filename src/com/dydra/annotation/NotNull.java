a70989da0ca7e03d723a46e87863c3450738e9fc
Arto Bendiken <arto@bendiken.net>
2011-07-25 12:49:33 +0200

    Defined project-local @NotNull and @Nullable annotations to enable annotating interfaces with null constraints in the vein of JSR 308.
    See: http://types.cs.washington.edu/jsr308/

---
+++ src/com/dydra/annotation/NotNull.java
@@ -0,0 +1,18 @@
+/* This is free and unencumbered software released into the public domain. */
+
+package com.dydra.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Designates that a field, return value, argument, or variable is
+ * guaranteed to be non-null.
+ */
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE})
+@Documented
+@Retention(RetentionPolicy.CLASS)
+public @interface NotNull {}
