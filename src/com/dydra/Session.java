5157be289e2650946211ff6ba773a34a848b2820
Arto Bendiken <arto@bendiken.net>
2011-07-26 14:20:05 +0200

    Renamed the com.dydra.Client class to com.dydra.Session.

---
+++ src/com/dydra/Session.java
@@ -0,0 +1,61 @@
+package com.dydra;
+
+import com.dydra.annotation.*;
+
+/**
+ * Represents a Dydra.com API session.
+ *
+ * @see http://docs.dydra.com/sdk/java
+ * @see http://docs.dydra.com/api/rpc
+ */
+public class Session {
+  /**
+   * The RPC endpoint URL.
+   */
+  public String url;
+
+  /**
+   * The API token, if any.
+   */
+  public String token;
+
+  /**
+   * The account name.
+   */
+  public String username;
+
+  /**
+   * The account password.
+   */
+  public String password;
+
+  /**
+   * Constructs an unauthenticated session.
+   */
+  public Session() {
+    this.token = null;
+    this.url   = Dydra.getPublicURL("rpc");
+  }
+
+  /**
+   * Constructs an authenticated session using the given API token.
+   *
+   * @param  token a valid API token
+   */
+  public Session(@NotNull String token) {
+    this.token = token;
+    this.url   = Dydra.getAuthenticatedURL(token, "rpc");
+  }
+
+  /**
+   * Constructs an authenticated session using the given HTTP credentials.
+   *
+   * @param  username a valid Dydra.com account name
+   * @param  password the account password
+   */
+  public Session(@NotNull String username, @NotNull String password) {
+    this.username = username;
+    this.password = password;
+    this.url      = Dydra.getAuthenticatedURL(username, password, "rpc");
+  }
+}
