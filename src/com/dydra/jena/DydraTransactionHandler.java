c24272f55c64e3644065b68e75bd66f9b036eaa4
Arto Bendiken <arto@bendiken.net>
2011-07-29 22:02:19 +0200

    Imported a scaffold for the com.dydra.jena.DydraTransactionHandler class.

---
+++ src/com/dydra/jena/DydraTransactionHandler.java
@@ -0,0 +1,46 @@
+/* This is free and unencumbered software released into the public domain. */
+
+package com.dydra.jena;
+
+import com.dydra.annotation.*;
+import com.hp.hpl.jena.graph.TransactionHandler;
+import com.hp.hpl.jena.graph.impl.TransactionHandlerBase;
+import com.hp.hpl.jena.shared.Command;
+
+/**
+ * @see http://docs.dydra.com/sdk/java/jena
+ * @see http://openjena.org/javadoc/com/hp/hpl/jena/graph/TransactionHandler.html
+ */
+public class DydraTransactionHandler extends TransactionHandlerBase implements TransactionHandler {
+  public DydraTransactionHandler() {
+    super();
+  }
+
+  @Override
+  public void abort() {
+    throw new UnsupportedOperationException("not implemented"); // TODO
+  }
+
+  @Override
+  public void begin() {
+    throw new UnsupportedOperationException("not implemented"); // TODO
+  }
+
+  @Override
+  public void commit() {
+    throw new UnsupportedOperationException("not implemented"); // TODO
+  }
+
+  @Override
+  public Object executeInTransaction(@NotNull final Command command) {
+    if (command == null)
+      throw new NullPointerException("command cannot be null");
+
+    return super.executeInTransaction(command);
+  }
+
+  @Override
+  public boolean transactionsSupported() {
+    return false; // TODO
+  }
+}
