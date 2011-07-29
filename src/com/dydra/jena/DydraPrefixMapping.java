d87362d7ddfb34bbb2490414ebf10bd7b44f2cc8
Arto Bendiken <arto@bendiken.net>
2011-07-29 23:46:07 +0200

    Imported a scaffold for the com.dydra.jena.DydraPrefixMapping class.

---
+++ src/com/dydra/jena/DydraPrefixMapping.java
@@ -0,0 +1,15 @@
+/* This is free and unencumbered software released into the public domain. */
+
+package com.dydra.jena;
+
+import com.dydra.annotation.*;
+import com.hp.hpl.jena.shared.PrefixMapping;
+import com.hp.hpl.jena.shared.impl.PrefixMappingImpl;
+
+/**
+ * @see http://docs.dydra.com/sdk/java/jena
+ * @see http://openjena.org/javadoc/com/hp/hpl/jena/shared/PrefixMapping.html
+ */
+public class DydraPrefixMapping extends PrefixMappingImpl implements PrefixMapping {
+  // TODO
+}
